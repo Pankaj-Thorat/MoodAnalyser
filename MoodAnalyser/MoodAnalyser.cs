b52d9b00123517271c25fe5454596842aa6c7192
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-07 23:57:40 +0530

    [Pankaj] Ability to analyse and respond the mood of given message.

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    class MoodAnalyser
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "Happy";
+            }
+            else if (message.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a MoodAnalyser object
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+            // Call the AnalyseMood function with a message
+            string message = "I am feeling happy today!";
+            string mood = moodAnalyser.AnalyseMood(message);
+
+            string message1 = "I am feeling sad today!";
+            string mood1 = moodAnalyser.AnalyseMood(message1);
+
+            string message2 = "I am feeling nothing today!";
+            string mood2 = moodAnalyser.AnalyseMood(message2);
+
+            // Print the determined mood
+            Console.WriteLine("Mood: " + mood);//happy
+            Console.WriteLine("Mood: " + mood1);//sad
+            Console.WriteLine("Mood: " + mood2);//happy
+        }
+    }
+}
